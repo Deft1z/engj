4fb879dbd769df34c5fdfde2d9e86e6edebbc387
xiehaolin <839427117@qq.com>
2024-07-19 19:24:37 +0800

    优化部分代码，补充注释
    1.控制台获取公司列表改为返回Resp对象
    2.提取运维列表分页查询接口请求ecc部分代码到service中

--- crm-mgr-provider/src/main/java/com/kge/energy/crm/operation/controller/OperationController.java
+++ crm-mgr-provider/src/main/java/com/kge/energy/crm/operation/controller/OperationController.java
@@ -3,11 +3,6 @@ package com.kge.energy.crm.operation.controller;
 import cn.hutool.core.bean.BeanUtil;
 import com.kge.energy.crm.common.go.ConvertToGoFormats;
 import com.kge.energy.crm.common.net.CommonResponse;
-import com.kge.energy.crm.external.ecc.req.Condition;
-import com.kge.energy.crm.external.ecc.req.EccReq;
-import com.kge.energy.crm.external.ecc.resp.EccMaintenance;
-import com.kge.energy.crm.external.ecc.resp.EccPageData;
-import com.kge.energy.crm.external.ecc.resp.EccResp;
 import com.kge.energy.crm.external.ecc.service.EccService;
 import com.kge.energy.crm.operation.req.OperationDetailReq;
 import com.kge.energy.crm.operation.req.OperationListReq;
@@ -20,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * 运维列表接口
@@ -41,37 +35,7 @@ public class OperationController {
     @ConvertToGoFormats
     @PostMapping("/external/getRecord")
     public CommonResponse<Object> getPage(@RequestBody OperationListReq req) throws NoSuchAlgorithmException {
-
-        // 构造ecc接口请求参数
-        EccReq eccReq = new EccReq();
-        eccReq.setPageNo(req.getPageNo());
-        eccReq.setPageSize(req.getPageSize());
-        // 构造查询条件
-        Condition condition = new Condition();
-        condition.setRiskRates(new String[]{"设备巡检", "设备试验", "设备维修", "设备检修、抢修作业"});
-        if (!req.getPhone().trim().equals("")) {
-            condition.setFirstPartyContactsPhone(req.getPhone());
-        }
-        eccReq.setCondition(condition);
-
-        EccResp<EccPageData<EccMaintenance>> eccResp = eccService.getMaintenanceList(eccReq);
-
-        // 转换attachment中的路径
-        Optional.ofNullable(eccResp)
-                .map(EccResp::getData)
-                .map(EccPageData::getList)
-                .map(list -> {
-                    list.forEach(e ->
-                            e.setAttactments(
-                                    e.getAttactments().stream()
-                                            .map(attachment -> attachment.setUrl(attachment.getUrl().replace(eccService.ECC_PREFIX, "")))
-                                            .collect(Collectors.toList())
-                            )
-                    );
-                    return list;
-                });
-
-        return CommonResponse.suc(eccResp);
+        return CommonResponse.suc(operationService.getPage(req));
     }
 
     /**
