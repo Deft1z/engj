90719c9c59897d3f613ff99e983ff5b3b23ad2dc
wangjihua <13288012316@163.com>
2024-08-19 11:53:35 +0800

    删除无用代码

--- crm-infrastructure/src/main/java/com/kge/energy/crm/repository/mapper/BResourceMapper.java
+++ crm-infrastructure/src/main/java/com/kge/energy/crm/repository/mapper/BResourceMapper.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kge.energy.crm.repository.entity.BResource;
 import com.kge.energy.crm.repository.entityext.param.SystemResourceParam;
 import com.kge.energy.crm.repository.entityext.param.UserResourceParam;
-import com.kge.energy.crm.repository.entityext.result.ResourcePermissionResult;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,8 +11,6 @@ import java.util.List;
  * 资源表(BResource)表数据库接口层
  */
 public interface BResourceMapper extends BaseMapper<BResource> {
-
-    List<ResourcePermissionResult> findMenu(@Param("userId") Integer userId);
 
     List<BResource> getSystemResources(SystemResourceParam param);
 
