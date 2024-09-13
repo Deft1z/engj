fee618d4c169bc8b4d9a378cbe736789f3e378e2
fangxiaoqing <1983699451@qq.com>
2024-09-13 09:57:05 +0800

    redis对连续输错账号密码计数并冻结账号1小时

--- crm-infrastructure/src/main/java/com/kge/energy/crm/common/constans/TokenConstant.java
+++ crm-infrastructure/src/main/java/com/kge/energy/crm/common/constans/TokenConstant.java
@@ -22,4 +22,14 @@ public class TokenConstant {
 
     public static final TimeUnit APPLET_EXPIRED_TIMEUNIT = TimeUnit.HOURS;
 
+    /*
+    * 连续登录失败次数限制
+    * */
+    public static final int MAX_LOGIN_ERROR_TIMES = 5;
+
+    public static final long LOGIN_ERROR_BAN_TIME = 1;
+
+    public static final TimeUnit LOGIN_ERROR_BAN_TIMEUNIT = TimeUnit.HOURS;
+
+    public static final String LOGIN_ERROR_CACHE_KEY = "login:error:%s";
 }
