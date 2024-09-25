6dec6274cc9bfc4472da31df8df0e8b1fb5d7e4a
fangxiaoqing <1983699451@qq.com>
2024-09-25 08:58:50 +0800

    组织管理添加类型字段

--- crm-infrastructure/src/main/java/com/kge/energy/crm/repository/entityext/result/OrgListResult.java
+++ crm-infrastructure/src/main/java/com/kge/energy/crm/repository/entityext/result/OrgListResult.java
@@ -31,6 +31,9 @@ public class OrgListResult {
     @Schema(description = "排序")
     private Integer sort;
 
+    @Schema(description = "组织类别:0-集团 1-公司 2-部门 3-项目组")
+    private Integer orgType;
+
     @Schema(description = "状态,0正常 1停用")
     private Integer status;
 
