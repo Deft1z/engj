536e70e13ca571e1dfb9de057b1b50838ee587a4
tangchenghui <2638497085@qq.com>
2024-09-27 16:12:11 +0800

    调整pc端和小程序端运维报告数据权限

--- crm-mgr-provider/src/main/java/com/kge/energy/crm/operation/service/OperationService.java
+++ crm-mgr-provider/src/main/java/com/kge/energy/crm/operation/service/OperationService.java
@@ -4,6 +4,8 @@ import cn.hutool.core.util.StrUtil;
 import com.kge.energy.crm.common.dto.UserInfoDto;
 import com.kge.energy.crm.common.util.AuthVerifyUtils;
 import com.kge.energy.crm.common.util.UserInfoContextUtils;
+import com.kge.energy.crm.enums.BizFunctionEnums;
+import com.kge.energy.crm.enums.DataPermissionRangeTypeEnums;
 import com.kge.energy.crm.external.ecc.req.Condition;
 import com.kge.energy.crm.external.ecc.req.EccReq;
 import com.kge.energy.crm.external.ecc.resp.EccMaintenance;
@@ -11,9 +13,13 @@ import com.kge.energy.crm.external.ecc.resp.EccPageData;
 import com.kge.energy.crm.external.ecc.resp.EccResp;
 import com.kge.energy.crm.external.ecc.service.EccService;
 import com.kge.energy.crm.operation.req.OperationListReq;
+import com.kge.energy.crm.permission.service.DataPermissionDomainService;
+import com.kge.energy.crm.repository.dao.BOrganizationDao;
 import com.kge.energy.crm.repository.dao.OmReportDao;
+import com.kge.energy.crm.repository.entity.BUser;
 import com.kge.energy.crm.repository.entityext.param.OperationParam;
 import com.kge.energy.crm.repository.entityext.result.OperationDetail;
+import com.kge.energy.crm.user.service.UserDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,7 +39,10 @@ import java.util.stream.Collectors;
 public class OperationService {
 
     private final EccService eccService;
+    private final UserDomainService userDomainService;
     private final OmReportDao dao;
+    private final BOrganizationDao bOrganizationDao;
+    private final DataPermissionDomainService dataPermissionDomainService;
 
     //集团领导
     @Value("${group.leaderPhones}")
@@ -48,20 +57,70 @@ public class OperationService {
         Condition condition = new Condition();
         condition.setRiskRates(new String[]{"设备巡检", "设备试验", "设备维修", "设备检修、抢修作业"});
 
-        //手机号搜索判断逻辑
-        UserInfoDto currentUserInfo = UserInfoContextUtils.getCurrentUserInfo();
-        List<String> leaderPhoneList = new ArrayList<>(Arrays.asList(leaderPhones));
+        DataPermissionRangeTypeEnums dataEnums = dataPermissionDomainService.getCurrentUserDataPermission(BizFunctionEnums.ECC_OM_REPORT_LIST);
         if(StrUtil.isBlank(req.getPhone())){
-            if(AuthVerifyUtils.isSuperAdmin() || leaderPhoneList.contains(currentUserInfo.getMobile())){
-                //超管和集团领导看所有
-                condition.setFirstPartyContactsPhone("");
-            } else {
-                condition.setFirstPartyContactsPhone(currentUserInfo.getMobile());
+            switch (dataEnums.getCode()) {
+                case 0, 1, 2:
+                    condition.setFirstPartyContactsPhone("");
+                    break;
+                case 3:
+                    String eccOrgCode = bOrganizationDao.getEccOrgCode(UserInfoContextUtils.getCurrentOrgId());
+                    condition.setSysCompanyCode(StrUtil.isNotBlank(eccOrgCode) ? eccOrgCode : "0");
+                    condition.setFirstPartyContactsPhone("");
+                    break;
+                default:
+                    //默认看当前用户手机号关联的数据
+                    String currentUserPhone = UserInfoContextUtils.getCurrentMobile();
+                    List<BUser> userList = userDomainService.findByPhone(currentUserPhone);
+                    List<Integer> userIdList = userList.stream().map(BUser::getUserId).toList();
+                    String firstPartyContactsPhone = userDomainService.findShareUser(userIdList, 3);
+
+                    //防止currentUserPhone和firstPartyContactsPhone都为空查到全部数据
+                    if(StrUtil.isBlank(currentUserPhone)){
+                        currentUserPhone = "0";
+                    }
+                    condition.setFirstPartyContactsPhone(StrUtil.isNotBlank(firstPartyContactsPhone) ? firstPartyContactsPhone : currentUserPhone);
             }
+
         } else {
-            // TODO: 手机号权限区分
-            condition.setFirstPartyContactsPhone(req.getPhone());
+            switch (dataEnums.getCode()) {
+                case 0, 1, 2:
+                    condition.setFirstPartyContactsPhone(req.getPhone());
+                    break;
+                case 3:
+                    String eccOrgCode = bOrganizationDao.getEccOrgCode(UserInfoContextUtils.getCurrentOrgId());
+                    condition.setSysCompanyCode(StrUtil.isNotBlank(eccOrgCode) ? eccOrgCode : "0");
+                    condition.setFirstPartyContactsPhone(req.getPhone());
+                    break;
+                default:
+                    //默认看当前用户手机号关联的数据
+                    String currentUserPhone = UserInfoContextUtils.getCurrentMobile();
+                    List<BUser> userList = userDomainService.findByPhone(currentUserPhone);
+                    List<Integer> userIdList = userList.stream().map(BUser::getUserId).toList();
+                    String firstPartyContactsPhone = userDomainService.findShareUser(userIdList, 3);
+
+                    //防止currentUserPhone和firstPartyContactsPhone都为空查到全部数据
+                    if(StrUtil.isBlank(currentUserPhone)){
+                        currentUserPhone = "0";
+                    }
+                    condition.setFirstPartyContactsPhone(StrUtil.isNotBlank(firstPartyContactsPhone) ? firstPartyContactsPhone : currentUserPhone);
+            }
         }
+
+        //手机号搜索判断逻辑
+//        UserInfoDto currentUserInfo = UserInfoContextUtils.getCurrentUserInfo();
+//        List<String> leaderPhoneList = new ArrayList<>(Arrays.asList(leaderPhones));
+//        if(StrUtil.isBlank(req.getPhone())){
+//            if(AuthVerifyUtils.isSuperAdmin() || leaderPhoneList.contains(currentUserInfo.getMobile())){
+//                //超管和集团领导看所有
+//                condition.setFirstPartyContactsPhone("");
+//            } else {
+//                condition.setFirstPartyContactsPhone(currentUserInfo.getMobile());
+//            }
+//        } else {
+//            // TODO: 手机号权限区分
+//            condition.setFirstPartyContactsPhone(req.getPhone());
+//        }
 
         eccReq.setCondition(condition);
 
