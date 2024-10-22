970afdb4a87e1bb20f05e1b4abf8d49118fcdc26
tangchenghui <2638497085@qq.com>
2024-10-22 11:08:57 +0800

    新增电鸿模块使能状态查询接口

--- crm-applet-provider/src/main/java/com/kge/energy/crm/dianhong/controller/DianhongController.java
+++ crm-applet-provider/src/main/java/com/kge/energy/crm/dianhong/controller/DianhongController.java
@@ -2,8 +2,10 @@ package com.kge.energy.crm.dianhong.controller;
 
 import com.kge.energy.crm.dianhong.resp.DhStatisticResp;
 import com.kge.energy.crm.dianhong.service.DianhongService;
+import com.kge.energy.dh.req.DeviceControlEnableReq;
 import com.kge.energy.dh.req.DeviceControlReq;
 import com.kge.energy.dh.req.DeviceEnableReq;
+import com.kge.energy.dh.resp.DeviceControlEnableResp;
 import com.kge.energy.dh.resp.DeviceControlResp;
 import com.kge.energy.dh.resp.DeviceEnableResp;
 import com.kge.platform.framework.common.net.CommonResult;
@@ -31,6 +33,11 @@ public class DianhongController {
     @PostMapping("/dianhong/setControlPercent")
     public CommonResult<DeviceControlResp> setControlPercent(@Valid @RequestBody DeviceControlReq req){
         return CommonResult.suc(dianhongService.setControlPercent(req));
+    }
+
+    @PostMapping("/dianhong/getControlEnable")
+    public CommonResult<DeviceControlEnableResp> getControlEnable(@Valid @RequestBody DeviceControlEnableReq req){
+        return CommonResult.suc(dianhongService.getControlEnable(req));
     }
 
 }
