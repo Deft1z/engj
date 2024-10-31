fa689f7fffe137885c06993fc785cc84ac7d3405
tangchenghui <2638497085@qq.com>
2024-10-31 14:50:59 +0800

    修改小程序短信跳转连接参数1

--- crm-infrastructure/src/main/java/com/kge/energy/crm/common/util/AppletLinkUtils.java
+++ crm-infrastructure/src/main/java/com/kge/energy/crm/common/util/AppletLinkUtils.java
@@ -12,17 +12,17 @@ public class AppletLinkUtils {
 
     //获取工单详情query
     public static String getFormDetailQuery(Integer formId) {
-        return "type=" + BIZ_TYPE_ORDER_DETAIL + "&path=" + ORDER_DETAIL_PATH + "&formId=" + formId;
+        return "type=" + BIZ_TYPE_ORDER_DETAIL + "&path=" + ORDER_DETAIL_PATH + "&detailId=" + formId;
     }
 
     //获取合同详情query
     public static String getContractDetailQuery(Integer contractId) {
-        return "type=" + BIZ_TYPE_CONTRACT_DETAIL + "&path=" + CONTRACT_DETAIL_PATH + "&contractId=" + contractId;
+        return "type=" + BIZ_TYPE_CONTRACT_DETAIL + "&path=" + CONTRACT_DETAIL_PATH + "&detailId=" + contractId;
     }
 
     //获取投诉建议详情query
     public static String getComplainDetailQuery(Integer complainId) {
-        return "type=" + BIZ_TYPE_COMPLAIN_DETAIL + "&path=" + COMPLAIN_DETAIL_PATH + "&complainId=" + complainId;
+        return "type=" + BIZ_TYPE_COMPLAIN_DETAIL + "&path=" + COMPLAIN_DETAIL_PATH + "&detailId=" + complainId;
     }
 
 }
